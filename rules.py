d0ccdbfa5062814ed3358edfa1afec06c2720eb9
Dayo Osikoya <alwaysdeone@gmail.com>
2015-08-27 19:04:35 +0100

    Add .gitignore and deploy shell script. Add authenticate and checksimul functions. Set DJANGO_SETTINGS_MODULE env var. Append billing project and site-packages to sys.path.

--- rules.py
+++ rules.py
@@ -10,59 +10,78 @@ Meraki Params example
 (('Acct-Session-Id', '"624874448299247292"'), ('Called-Station-Id', '"00-18-0A-F2-DE-20:Radius test"'), ('Calling-Station-Id', '"48-D2-24-43-A6-C1"'), ('Framed-IP-Address', '172.31.3.83'), ('NAS-Identifier', '"Cisco Meraki cloud RADIUS client"'), ('NAS-IP-Address', '108.161.147.120'), ('NAS-Port', '0'), ('NAS-Port-Id', '"Wireless-802.11"'), ('NAS-Port-Type', 'Wireless-802.11'), ('Service-Type', 'Login-User'), ('User-Name', '"hyellis@gmail.com"'), ('User-Password', '"12345"'), ('Attr-26', '0x000073e7010f446a756e676c65204851203032'))
 """
 
+import os
+import sys
+
+sys.path.insert(1, "/Users/deone/src/billing/lib/python2.7/site-packages")
+sys.path.insert(1, "/Users/deone/src/billing/billing")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billing.settings")
+
 import radiusd
+import django
+
+from accounts.models import Radcheck
 
 def instantiate(p):
-  print "*** instantiate ***"
-  print p
+    print "*** instantiate ***"
+    # print p
 
 def authorize(p):
-  print "*** authorize ***"
-  print
-  radiusd.radlog(radiusd.L_INFO, '*** radlog call in authorize ***')
-  print
-  print p
-  return radiusd.RLM_MODULE_REJECT
+    print "*** authorize ***"
+    # print
+    # radiusd.radlog(radiusd.L_INFO, '*** radlog call in authorize ***')
+    # print
+    print p
+    return radiusd.RLM_MODULE_OK
+
+def authenticate(p):
+    print "*** authenticate ***"
+    print p
+    return radiusd.RLM_MODULE_OK
 
 def preacct(p):
-  print "*** preacct ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** preacct ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def accounting(p):
-  print "*** accounting ***"
-  radiusd.radlog(radiusd.L_INFO, '*** radlog call in accounting (0) ***')
-  print
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** accounting ***"
+    # radiusd.radlog(radiusd.L_INFO, '*** radlog call in accounting (0) ***')
+    # print
+    # print p
+    return radiusd.RLM_MODULE_OK
+
+def checksimul(p):
+    print "*** checksimul ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def pre_proxy(p):
-  print "*** pre_proxy ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** pre_proxy ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def post_proxy(p):
-  print "*** post_proxy ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** post_proxy ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def post_auth(p):
-  print "*** post_auth ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** post_auth ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def recv_coa(p):
-  print "*** recv_coa ***"
-  print p
-  return radiusd.RLM_MODULE_OK
+    print "*** recv_coa ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def send_coa(p):
-  print "*** send_coa ***"
-  print p
-  return radiusd.RLM_MODULE_OK
-
+    print "*** send_coa ***"
+    # print p
+    return radiusd.RLM_MODULE_OK
 
 def detach():
-  print "*** goodbye from example.py ***"
-  return radiusd.RLM_MODULE_OK
-
+    print "*** goodbye from example.py ***"
+    return radiusd.RLM_MODULE_OK
