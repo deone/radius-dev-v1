1068beb8bd7baa20e61a385057aebb013de307ff
Dayo Osikoya <alwaysdeone@gmail.com>
2015-08-29 15:35:30 +0100

    Update test to reflect file name change.

--- tests.py
+++ tests.py
@@ -2,24 +2,24 @@
 
 import unittest
 
-import example
+import rules 
 
 class AuthorizeTestCase(unittest.TestCase):
 
     def setUp(self):
         self.p = (('User-Name', '"alwaysdeone@gmail.com"'), ('User-Password', '"12345"'), ('NAS-IP-Address', '192.168.8.102'), ('NAS-Port', '0'), ('Message-Authenticator', '0x7edbbcb48daa747ef293a0ba548c1f6c'))
-        self.user = example.get_user(self.p)
+        self.user = rules.get_user(self.p)
         self.user.is_active = True
         self.user.save()
 
     def test_success(self):
-        result = example.authorize(self.p)
+        result = rules.authorize(self.p)
         self.assertEqual(result, 2)
 
     def test_fail(self):
         self.user.is_active = False
         self.user.save()
-        result = example.authorize(self.p)
+        result = rules.authorize(self.p)
         self.assertEqual(result, 0)
 
 suite = unittest.TestSuite([AuthorizeTestCase('test_success'), AuthorizeTestCase('test_fail')])
