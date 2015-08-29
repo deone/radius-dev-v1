f9416141552e88218ee850695ad88a6e00e9187f
Dayo Osikoya <alwaysdeone@gmail.com>
2015-08-29 12:00:14 +0100

    Undo make_env(). Doesn't work.

--- example.py
+++ example.py
@@ -11,18 +11,17 @@ import subprocess
 
 import radiusd
 
-def make_env():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billing.settings")
-    hostname = subprocess.check_output('hostname')[:-1]
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billing.settings")
+hostname = subprocess.check_output('hostname')[:-1]
 
-    if not hostname.startswith('billing'):
-        sys.path.insert(1, "/Users/deone/src/billing/lib/python2.7/site-packages")
-        sys.path.insert(1, "/Users/deone/src/billing/billing")
-    else:
-        sys.path.insert(1, "/root/billing/lib/python2.7/site-packages")
-        sys.path.insert(1, "/root/billing/billing")
+if not hostname.startswith('billing'):
+    sys.path.insert(1, "/Users/deone/src/billing/lib/python2.7/site-packages")
+    sys.path.insert(1, "/Users/deone/src/billing/billing")
+else:
+    sys.path.insert(1, "/root/billing/lib/python2.7/site-packages")
+    sys.path.insert(1, "/root/billing/billing")
 
-    from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 def instantiate(p):
     print "*** instantiate ***"
@@ -35,7 +34,6 @@ def authorize(p):
     # print
     # print p
     # return radiusd.RLM_MODULE_OK
-    make_env()
     params = dict(p)
     username = params['User-Name'][1:-1]
     user = User.objects.get(username__exact=username)
